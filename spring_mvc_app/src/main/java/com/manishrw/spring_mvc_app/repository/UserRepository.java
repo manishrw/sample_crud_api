e1fb3c3c7514dda980fad7453d3ad7170d02e948
manish wadhwani <manishrw@users.noreply.github.com>
2022-10-11 18:08:50 +0000

    Removed old app dir

--- spring_mvc_app/src/main/java/com/manishrw/spring_mvc_app/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.manishrw.spring_mvc_app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.manishrw.spring_mvc_app.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    
-}
